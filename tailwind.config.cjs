cb80eef92a70832b8028d4af93f5efda77642aa5
Bethany Mattern <bethany.a.cable@gmail.com>
2023-01-17 12:39:53 -0500

    Setup Tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./Client/index.html', './Client/src/**/*.{js, ts, jsx, tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

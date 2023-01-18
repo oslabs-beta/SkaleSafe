8b6d794ba3307c491339941e21443d81b91834a0
daniel-doody <daniel.doody@ymail.com>
2023-01-18 16:48:48 -0500

    new cluster form created, backend handling complete

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import FileUploader from './components/FileUploader';
+import AddCluster from './components/addCluster/AddCluster';
+import NewCluster from './components/addCluster/NewCluster';
+
 function App() {
   return (
     <div className=''>
       <h1 className='text-3xl font-bold underline'>This is my test heading</h1>
+      {/* <AddCluster /> */}
     </div>
   );
 }

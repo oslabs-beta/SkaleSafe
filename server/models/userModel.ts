03a107c76844da2ff07b6ca5e4c5d978635466b8
Bethany Mattern <bethany.a.cable@gmail.com>
2023-01-18 17:17:20 -0500

    Add dotenv into userSchema

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -1,5 +1,8 @@
 import { ErrorRequestHandler, NextFunction } from "express";
 
+import dotenv from 'dotenv';
+
+dotenv.config();
 const mongoose = require('mongoose');
 const { DB_URI } = process.env;
 

4267b7c0ca6aec9d326cd5a961dcc00bf29bfad1
daniel-doody <daniel.doody@ymail.com>
2023-01-18 13:23:34 -0500

    file upload  created front/back, concurrently configured

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import FileUploader from './components/FileUploader';
+function App() {
+  return (
+    <div className=''>
+      <h1 className='text-3xl font-bold underline'>This is my test heading</h1>
+    </div>
+  );
+}
+
+export default App;
--- server/server.ts
+++ server/server.ts
@@ -5,8 +5,12 @@ import dotenv from 'dotenv';
 import process from 'process';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import multer from 'multer';
 dotenv.config();
 const PORT = process.env.PORT || 3002;
+
+// allows backend to get file:
+const upload = multer({ dest: 'uploads/' });
 
 const app = express();
 app.use(bodyParser.json());
@@ -21,9 +25,17 @@ app.get('/database', (req, res) => {
   res.send('You have reached the database route');
 });
 
-app.get('/api', (req, res) => {
-  // test api route
-  res.send('You have reached the API route');
+// UPLOAD:
+// retrieve files uploaded by user:
+app.post('/upload', upload.single('file'), (req, res) => {
+  try {
+    // access the file as req.file
+    console.log(req.file);
+    res.send({ message: 'File uploaded successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 });
 
 app.listen(PORT, () => {

140c0fff47186930351681ce4787089c8abde3bb
Bethany Mattern <bethany.a.cable@gmail.com>
2023-01-21 12:52:50 -0500

    Add more routes to user file and remove db connection in userModel

--- server/routes/user/userRouter.ts
+++ server/routes/user/userRouter.ts
@@ -1,8 +1,24 @@
 import express, { Request, Response } from "express";
+
+import userController from "../../controllers/userController";
+
 const router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
-    res.status(200).send('This is the user route')
+
+router.get('/signin', (req: Request, res: Response) => {
+    res.status(200).send('This is the user route');
+});
+
+router.get('/signup', (req: Request, res: Response) => {
+   res.status(200).send('This is the user route');
 })
+
+router.post('/signup', userController.createUser, (req: Request, res: Response) => {
+    // res.status(200).send()
+});
+
+router.post('/signup', userController.createUser, (req: Request, res: Response) => {
+    // res.status(200).send()
+});
 
 export default router;

530b9af0b7fcf01534e76aa0bc3a0bc225ccf45c
“KelvinShamy” <kelvinshamy@gmail.com>
2023-01-21 12:39:07 -0500

    set up dashboard and starter components

---
+++ client/src/components/alerts/Alets.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Alerts = (props: Props) => {
+  return (
+    <div>Alerts</div>
+  )
+}
+
+export default Alerts
---
+++ client/src/components/profile/Profile.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const Profile = (props: Props) => {
+  return (
+    <div>Profile</div>
+  )
+}

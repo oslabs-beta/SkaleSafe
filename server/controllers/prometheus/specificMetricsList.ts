0dc96cd9c164fd3de0cd429bc733a68a738c514f
daniel-doody <daniel.doody@ymail.com>
2023-01-23 15:20:30 -0500

    specific metric prom query added

--- server/controllers/prometheus/specificMetricsList.ts
+++ server/controllers/prometheus/specificMetricsList.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import axios from 'axios';
 
+// LIST
 const specificMetricsList = async (
   req: Request,
   res: Response,
@@ -15,9 +16,9 @@ const specificMetricsList = async (
     ];
     let metricsData: any = {};
     for (let metric of metrics) {
-      const query = `{__name__=~"${metric}"}`;
+      const query = `{__name__=~"${metric}", job="prom-job"}`;
       const { data } = await axios.get(
-        `http://localhost:9090/api/v1/query?query=${query}`
+        `http://localhost:8080/api/v1/query?query=${query}`
       );
       metricsData[metric] = data.data.result;
     }
@@ -29,4 +30,25 @@ const specificMetricsList = async (
   next();
 };
 
-export default specificMetricsList;
+// SINGLE METRIC
+const specificMetric = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const metric = 'kube_pod_labels';
+    const query = `{__name__=~"${metric}", job="prom-job"}`;
+    const { data } = await axios.get(
+      `http://localhost:8080/api/v1/query?query=${query}`
+    );
+    const metricsData = { [metric]: data.data.result };
+    res.json(metricsData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error fetching metrics');
+  }
+  next();
+};
+
+export { specificMetricsList, specificMetric };

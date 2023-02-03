39d0e8a431ee926916788c77042291b53cccc37f
Bethany Mattern <bethany.a.cable@gmail.com>
2023-02-03 13:15:09 -0500

    Start email implementation

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,15 +1,16 @@
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 import React from "react";
+import { SiGmail } from "react-icons/si"
 
 const Footer = () => {
     return (
-        <div id="footer" className="h-20 w-screen bg-sapphire-blue/30 flex flex-row justify-between items-center px-20">
-            <ul className='text-honeydew'>
+        <div id="footer" className="h-20 w-screen bg-honeydew/10 flex flex-row justify-between items-center px-20 text-honeydew">
+            <ul className='text-sm'>
                 <li>@2023 SkaleSafe Inc.</li>
                 <li>All rights reserved</li>
             </ul>
-            <ul className="w-10 flex flex-row justify-between">
+            <ul className="flex flex-row flex-wrap justify-between items-center">
                 {[[<AiFillGithub size={30} className='fill-honeydew' />, "https://github.com/oslabs-beta/SkaleSafe#readme"], [<AiFillLinkedin size={30} className="fill-honeydew"/>, "https://www.linkedin.com/company/skalesafe"]].map( ([mediaTag, link]) => (
                     <li>
                         <a href={link}>
@@ -17,6 +18,9 @@ const Footer = () => {
                         </a>
                     </li>
                 ))}
+                {/* <li>
+                    skalesafeteam@gmail.com
+                </li> */}
             </ul>
         </div>
     )

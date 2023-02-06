0809c060bba7f9048b5d297890a3cdad3d716774
sxhanx <sxhanx@gmail.com>
2023-02-05 19:59:37 -0500

    Intergrate interface and refactor fetch request Co-authored-by: Kelvin Shamy <kelvinshamy@gmail.com> Co-authored-by: Bethany Mattern <bethany.a.cable@gmail.com>

--- client/src/components/metrics/clusterMetrics/ClusterMetrics.tsx
+++ client/src/components/metrics/clusterMetrics/ClusterMetrics.tsx
@@ -1,4 +1,6 @@
 import { clusterData } from './ClusterData.ts';
+import { useEffect, useState } from "react";
+import dashboardState  from "../../../interfaces/dashboardState";
 
 type Props = {};
 
@@ -7,7 +9,30 @@ type Props = {};
 // grafana Port -> 8888 -> this part of URL will change... grab grafana port from db
 
 const ClusterMetrics = (props: Props) => {
+  // use username from local storage to
+  const username = localStorage.getItem('username')
+  const [userData, setUserData] = useState<dashboardState>({
+    grafPort: '',
+    customUID: '', 
+    alertsUID: '',
+  });
   
+  // fetch alertsUID and grafPort from DBs
+  const handleFetchData = async () => {
+    try {
+      const userResponse = await fetch(`http://localhost:3000/graf/clustermetrics?username=${username}`)
+      const data = await userResponse.json();
+      setUserData(data);
+    } catch (err) {
+      console.error('User alerts metrics could not be retrieved');
+      return err;
+    }
+  }
+
+  useEffect(() => {
+    handleFetchData();
+  },[])
+
   return (
     <div
       id='clustertab'
@@ -20,7 +45,8 @@ const ClusterMetrics = (props: Props) => {
         {clusterData.map((item: any) => (
           <iframe
           //the or3xtlo4k should be coming from DB
-          src={`http://localhost:8888/graf/d-solo/or3xtlo4k/Custom-Dashboard?orgId=1&panelId=${item.ID}`}
+          src={`${userData.grafPort}/graf/d-solo/${userData.customUID}/Custom-Dashboard?orgId=1&panelId=${item.ID}`}
+          // src={`http://localhost:8888/graf/d-solo/or3xtlo4k/Custom-Dashboard?orgId=1&panelId=${item.ID}`}
           
           width={item.width}
           height={item.height}

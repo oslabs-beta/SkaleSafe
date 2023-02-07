6387374affa3dd1c728135d0fff65f3bd537f64b
Bethany Mattern <bethany.a.cable@gmail.com>
2023-02-07 12:33:51 -0500

    Add eslint and prettier configuration

--- client/src/containers/App.tsx
+++ client/src/containers/App.tsx
@@ -6,7 +6,6 @@ import Footer from '../components/Footer/Footer';
 import HomeContainer from '../containers/HomeContainer';
 import Navbar from '../components/Navbar/Navbar';
 import Profile from '../components/Profile/Profile';
-import SignIn from '../components/Signin/SignIn';
 
 //Links are setup to allow only <routes> to change; the whole app is not re-rendered
 //Nested routes
@@ -27,10 +26,6 @@ const App = () => {
           <Route path='/home' element={<HomeContainer />} />
 
           <Route path='/dashboard' element={<Dashboard />} />
-
-
-          {/* switch paths for scalingmetrics and alerts to dashnoard href made in dashboard file */}
-          <Route path='/signin' element={<SignIn />} />
 
           <Route path='*' element={<Error />} />
         </Routes>
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +11,15 @@ export default defineConfig({
     port: 4000,
     open: '/',
   },
+  build: {
+    minify: true,
+    rollupOptions: {
+      input:{
+        main: resolve(__dirname, './client/index.html')
+      }
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  }
 });

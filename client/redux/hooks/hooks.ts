44fa98c2cad6124d0329f530e2d633b0ffa5b51b
Bethany Mattern <67919858+bethanycable@users.noreply.github.com>
2023-02-07 13:21:17 -0500

    Delete client/redux/hooks directory

--- client/redux/hooks/hooks.ts
+++
@@ -1,7 +0,0 @@
-import type { AppDispatch, RootState } from '../store';
-import { useDispatch, useSelector } from "react-redux";
-
-import type { TypedUseSelectorHook } from "react-redux"
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;

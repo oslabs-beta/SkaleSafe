d40dc1b5555a412ea25721161dd37a2bbc754df4
Bethany Mattern <bethany.a.cable@gmail.com>
2023-02-07 15:52:23 -0500

    Refactor spacing on files and update About

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React from 'react';
 
-const Home =() => {
-
-    return (
-        <div id="home" className="h-[48rem] w-screen bg-gradient-to-tr from-teal-blue flex flex-row items-center justify-evenly">
-            <h1 id='eleh1' className='text-6xl text-primary-color leading-relaxed tracking-wide m-0 p-0'>Auto-scaling<br/> made <span id='elespan' className='text-honeydew underline underline-offset-4 capitalize'>Better</span></h1>
-                <img id='homeLogo' className="w-2/5" src='../../../assets/SkaleSafe-color.svg' alt='A fish inside a shield. SkaleSafe' />
-        </div>
-    )
+function Home() {
+  return (
+    <div
+      id="home"
+      className="h-[48rem] w-screen bg-gradient-to-tr from-teal-blue flex flex-row items-center justify-evenly"
+    >
+      <h1
+        id="eleh1"
+        className="text-6xl text-primary-color leading-relaxed tracking-wide m-0 p-0"
+      >
+        Kubernetes
+        <br />
+        Auto-scaling
+        <br />
+        <span
+          id="elespan"
+          className="font-semibold text-honeydew underline underline-offset-4 capitalize"
+        >
+          Demystified
+        </span>
+      </h1>
+      <img
+        id="homeLogo"
+        className="w-2/5 max-w-lg"
+        src="../../../assets/SkaleSafe-color.svg"
+        alt="A fish inside a shield. SkaleSafe"
+      />
+    </div>
+  );
 }
 
 export default Home;

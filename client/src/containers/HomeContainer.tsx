03a023ca97b4b2ea0297666082e5cd2175457717
Bethany Mattern <bethany.a.cable@gmail.com>
2023-02-08 17:27:26 -0500

    Remove extra files

--- client/src/containers/HomeContainer.tsx
+++ client/src/containers/HomeContainer.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable import/order */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
 import About from '../components/Home/About/About';
 import Demo from '../components/Home/Demo/Demo';
 import Documentation from '../components/Home/Documentation/Documentation';
 import Footer from '../components/Footer/Footer';
 import Home from '../components/Home/Home';
 import MeetTeam from '../components/Home/MeetTeam/MeetTeam';
+import React from 'react';
 
 function HomeContainer() {
   return (

e2854bd339fd04d3d1cd3a9e1edbb9d31cc217ba
Bethany Mattern <bethany.a.cable@gmail.com>
2023-02-08 20:10:45 -0500

    Update Demo file

--- client/src/components/Home/Demo/Demo.tsx
+++ client/src/components/Home/Demo/Demo.tsx
@@ -14,7 +14,7 @@ function Demo() {
       </h1>
       <div className="flex flex-col items-center gap-4 text-honeydew text-lg lg:text-3xl mx-20">
         <div className="flex flex-row items-center self-start">
-          <video autoPlay loop className="w-2/3 max-w-lg ">
+          <video autoPlay loop muted className="w-2/3 max-w-lg ">
             <track default kind="captions" srcLang="en" />
             <source
               src="../../../../assets/login-and-scale-metrics.mp4"
@@ -29,7 +29,7 @@ function Demo() {
           <p className="mr-5">
             See how many alerts you have and watch how active they are!
           </p>
-          <video autoPlay loop className="w-2/3 max-w-lg">
+          <video autoPlay loop muted className="w-2/3 max-w-lg">
             <track default kind="captions" srcLang="en" />
             <source
               src="../../../../assets/alert-metrics.mp4"
@@ -38,7 +38,7 @@ function Demo() {
           </video>
         </div>
         <div className="flex flex-row items-center self-start">
-          <video autoPlay loop className="w-2/3 max-w-lg">
+          <video autoPlay loop muted className="w-2/3 max-w-lg">
             <track default kind="captions" srcLang="en" />
             <source src="../../../../assets/kubeview.mp4" type="video/mp4" />
           </video>
@@ -50,7 +50,7 @@ function Demo() {
           <p className="mr-5">
             You can also monitor your Kubernetes cluster health!
           </p>
-          <video autoPlay loop className="w-2/3 max-w-lg">
+          <video autoPlay loop muted className="w-2/3 max-w-lg">
             <track default kind="captions" srcLang="en" />
             <source
               src="../../../../assets/cluster-health-metrics.mp4"

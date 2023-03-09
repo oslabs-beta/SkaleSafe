59d1feef4f463816fb0eaf832a590268e83d8cbc
MetaBrian <114273788+MetaBrian@users.noreply.github.com>
2023-03-09 17:26:04 -0500

    updated modeswitch file

--- electron/ELECTRONMAIN.tsx
+++ electron/ELECTRONMAIN.tsx
@@ -1,8 +1,11 @@
+const path = require('path')
 const { BrowserWindow, app } = require('electron');
+
+
+
 
 function createWindow(){
   const win = new BrowserWindow({
-    // width: 1920,
     width: 1440,
     height: 1080,
     backgroundColor: 'white',
@@ -12,23 +15,9 @@ function createWindow(){
       contextIsolation: true,
     },
   });
-  win.loadURL('http://localhost:4000');
+  win.loadFile(path.join(__dirname , '../Client/index.html'));
 }
 
 app.whenReady().then(createWindow);
 
 
-//Best practice across all platforms
-//  app.on('activate', () => {
-//   if (BrowserWindow.getAllWindows().length === 0) {
-//     createWindow();
-//   }
-// });
-// });
-
-//For exiting the app on Macs
-// app.on('window-all-closed', () => {
-// if (process.platform !== 'darwin') {
-//   app.quit();
-// }
-// });
